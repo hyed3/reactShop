d24ebfbe233db5982749e080afa7ee6a04a914fe
hyed3 <shonlucy@naver.com>
2021-03-10 02:25:51 +0900

    반복코드 컴포넌트화

--- src/App.js
+++ src/App.js
@@ -1,3 +1,5 @@
+  /* eslint-disable*/ //eslint warning 무시하기 
+
 import { Navbar,Nav,NavDropdown,Button,Jumbotron} from 'react-bootstrap';
 import React, { useState } from 'react';
 import Data from './data.js';
@@ -41,31 +43,47 @@ function App() {
     <Button variant="primary">Learn more</Button>
   </p>
 </Jumbotron>
-
+    
+      
+    
     <duv className="container"> {/* bootstrap 문법, container: 좌우여백을 적당히 주겠다 */}
       <div className="row"> {/* bootstrap 문법, row : 칼럼을 12조각으로 나누겠다 */}
-        <div className="col-md-4"> {/* bootstrap 문법, col-4: 한개의 div를 4조각 크기로 쓰겠다  */}
+
+      {
+        상품.map(function(a,i){  //()=>{ } 이렇게 써도됨, i만 쓰면 안된다 데이터를 담는 a 도 꼭서야됨
+          return <Content 상품={a} i={i} key={i}/>  //a= 상품[i], 반복문은 key={i}를 꼭 사용해야한다!
+        })
+      }
+      
+      {/* <Content 상품={상품[0]} />  // <컴포넌트 보낼이름={전송할state} />  */}
+    
+       
+        {/* <div className="col-md-4">  //bootstrap 문법, col-4: 한개의 div를 4조각 크기로 쓰겠다 
+                                        // bootstrap 문법, 가운데 md는 모바일 환경
         <img src="/images/lovecat.jpg" width="100%"/>
         <h4>{ 상품[0].title }</h4>
         <p>{ 상품[0].content} & { 상품[0].price }</p>
-        </div>
-        <div className="col-md-4">
-        <img src="/images/bae.jpg" width="100%"/>
-        <h4>{ 상품[1].title }</h4>
-        <p>{ 상품[1].content} & { 상품[1].price}</p>
-        </div> {/* bootstrap 문법, 가운데 md는 모바일 환경*/}
-        <div className="col-md-4">
-        <img src="/images/cute.jpg" width="100%"/>
-        <h4>{ 상품[2].title}</h4>
-        <p>{ 상품[2].content} & {상품[2].price}</p>
-        </div>
+        </div> */}
 
+        
       </div>
 
     </duv>
 
     </div>
   );
+
 }
+
+function Content(props){
+  return (
+    <div className="col-md-4">
+        <img src={ "/images/"+props.i+".jpg" } width="100%"/>  {/* src=""에 데이터 바인딩을 하려면 {} 쳐주기 */}
+        <h4>{props.상품.title}</h4>
+        <p>{props.상품.content}& {props.상품.price}</p>
+        </div> 
+  )
+} //content end
+
 
 export default App;
